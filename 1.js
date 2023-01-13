2a8fbd3f9f5a6f934c8ce1725d2182c0102bd7e8
Speedwagon213 <122610796+Speedwagon213@users.noreply.github.com>
2023-01-13 13:47:00 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,25 @@
+let el = document.getElementById('enter_form');
+el.addEventListener("submit", checkForm);
+function checkForm(event){
+    event.preventDefault();
+    let username = el.UserName.value;
+    let pass = el.pass.value;
+    let error = "";
+    if(username == "" || pass == ""){
+        error = "fill in all the fields";
+    }
+    else if(username.length < 3){
+        error = "few characters in the Username";
+    }
+    else if(pass.length < 7){
+        error = "few characters in the Password";
+    }
+    if(error != ""){
+        document.getElementById('error').innerHTML = error;
+    }
+    else{
+        alert("Welcome " + username + "!!!");
+        window.location = '1.html';
+    }
+
+}
